9c6f6c6f5df1abf165322cbe9c4b592b0f48aaa4
gyojinK <gyojink@gyojinKui-MacBookAir.local>
2023-05-17 23:35:57 +0900

    Use Stack

---
+++ JS/쇠막대기.js
@@ -0,0 +1,25 @@
+const r = '(((()(()()))(())()))(()())'
+
+let answer = 0;
+let stack = [];
+let idx = 0;
+while(idx < r.length){
+    let t = r.charAt(idx);
+    let elem = {value: t, i: idx};
+    stack.push(elem);
+    // console.log(stack);
+    if(stack[stack.length-1].value == ')' && stack[stack.length-2].value == '(' 
+        && stack[stack.length-1].i - 1 === stack[stack.length-2].i){
+        answer += stack.length - 2;
+        stack.pop();
+        stack.pop();
+    }else if(stack[stack.length-1].value == ')' && stack[stack.length-2].value == '('
+        && stack[stack.length-1].i - 1 !== stack[stack.length-2].i){
+        answer += 1;
+        stack.pop();
+        stack.pop();
+    }
+    idx++;
+}
+
+console.log(answer);
